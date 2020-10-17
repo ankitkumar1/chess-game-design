c0a8e88ae8e8b402c7b19db76aef4cacd0770145
ankitk3 <ankitk3@ANKITK3-TTYDJ.partnet.cisco.com>
2020-10-17 08:06:11 +0530

    Project Created for designing chess game.

---
+++ chess-game/src/com/ankit/chessboard/BootStrap.java
@@ -0,0 +1,13 @@
+package com.ankit.chessboard;
+
+/**
+ * Entry point to the project. It will have the main method which will call the required service
+ * 
+ * @author Ankit
+ * */
+public class BootStrap {
+
+	public static void main(String[] args) {
+		System.out.println("Test Application!");
+	}
+}
